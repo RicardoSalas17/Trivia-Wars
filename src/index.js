cbd43ae89af64ec606a78eb2a24b35de7396236c
RicardoSalas17 <ricardosalas.1716@gmail.com>
2020-12-07 22:04:30 -0600

    Se agrego archivo trivia.js para trabajar en el generador de trivia y se habilito axios para poder comenzar las peticiones a la API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles/main.scss';
+import axios from 'axios';
+
+// class Trivia {}
+const init = () => { 
+    axios.get('https://opentdb.com/api.php?amount=10')
+        .then(response => {
+            const questions = response.data.results;
+            console.log('questions: ', questions);
+        })
+        .catch(function (error) {
+            alert('No funciono u_U');
+            console.log(error);
+        });
+    }
+    init();
