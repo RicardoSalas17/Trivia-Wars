4dda843f107a0df44538b13f8472c01e4e9b9ccb
RicardoSalas17 <ricardosalas.1716@gmail.com>
2021-01-22 12:11:50 -0600

    Se agrego timer a las preguntas

--- src/trivia.js
+++ src/trivia.js
@@ -1,4 +1,6 @@
 const questionContainer = document.createElement("div");
+let time= document.createElement("h1");
+let clockCounter = 1
 class Trivia {
   constructor(trivia = [], difficulty) {
     this.trivia = trivia;
@@ -20,13 +22,13 @@ class Trivia {
         ind++;
       });
       if (element.difficulty == "hard") {
-        timeDif = 0.1;
+        timeDif = 11;
         pointDif = 100;
       } else if (element.difficulty == "easy") {
-        timeDif = 1;
+        timeDif = 21;
         pointDif = 200;
       } else if (element.difficulty == "medium") {
-        timeDif = 0.5;
+        timeDif = 16;
         pointDif = 150;
       }
       this.round[index] = {
@@ -64,6 +66,31 @@ class Trivia {
     });
     this.showTrivia();
   }
+  timeQuestion(data){
+    let times =this.round[this.questionShow].time
+     const timer =()=> {
+      if(clockCounter == data.number){
+    if(times > 0){
+      if(times == 0){
+        times  = 59;
+      }
+      else{
+        times  = times  - 1;
+      }
+      if(times .toString().length == 1){
+        times  = "0" + times ;
+      }
+      time.innerHTML = times ;
+      setTimeout(timer, 1000);
+    }
+    else{
+      this.gameCheck(this.questionShow);
+    }
+  }else if(data.number == 10){ 
+  }
+  }
+   timer()
+  }
   showTrivia() {
     const htmlKeyH = this.round[this.questionShow].htmlElements.hElement;
     questionContainer.appendChild(htmlKeyH);
@@ -87,42 +114,59 @@ class Trivia {
     htmlKeyB.forEach((ans) => {
       questionContainer.appendChild(ans);
     });
+    
+    questionContainer.appendChild(time);
+    this.timeQuestion(this.round[this.questionShow]);
   }
   gameCheck(ind) {
     if (ind < this.round.length - 1) {
       let elementKeys = this.round[ind].htmlElements;
       let correctAnswer = this.round[ind].correctAnswer;
       let chosenAnswer = this.round[ind].chooseAnswer;
+      console.log('chosenAnswer: ', chosenAnswer);
       let points = this.round[ind].points;
       if (correctAnswer == chosenAnswer) {
-          console.log('chosenAnswer: ', chosenAnswer);
-          console.log('correctAnswer: ', correctAnswer);
-          console.log("Correcto    +", points);
+          // console.log('chosenAnswer: ', chosenAnswer);
+          // console.log('correctAnswer: ', correctAnswer);
+          // console.log("Correcto    +", points);
           this.marker += points;
-          console.log("Marker", this.marker);
-        }else{
-            console.log('chosenAnswer: ', chosenAnswer);
-            console.log('correctAnswer: ', correctAnswer);
-            console.log("InCorrecto ----", points);
+          // console.log("Marker", this.marker);
+        }else if(chosenAnswer===undefined){
+          // console.log("Se acabo el tiempo")
+          this.round[ind].chooseAnswer = "End Time"
+        }
+        else{
+            // console.log('chosenAnswer: ', chosenAnswer);
+            // console.log('correctAnswer: ', correctAnswer);
+            // console.log("InCorrecto ----", points);
       }
       questionContainer.removeChild(elementKeys.hElement);
+      questionContainer.removeChild(time);
       elementKeys.ansHtml.forEach((element) => {
         questionContainer.removeChild(element);
       });
-      this.questionShow++;
+      if(this.questionShow==0||this.questionShow<10){
+        this.questionShow++;
+      }
+      if(this.questionShow >= 1){
+        clockCounter++
+      }
       this.showTrivia();
     } else {
       let elementKeys = this.round[ind].htmlElements;
       questionContainer.removeChild(elementKeys.hElement);
+      questionContainer.removeChild(time);
       elementKeys.ansHtml.forEach((element) => {
         questionContainer.removeChild(element);
       }) 
       if(this.marker >= 1000){
           console.log("WIN");
           console.log("Marker", this.marker);
+          console.log("round", this.round);
         }else{
-            console.log("LOSE");
-            console.log("Marker", this.marker);
+          console.log("LOSE");
+          console.log("Marker", this.marker);
+          console.log("round", this.round);
       }
     }
   }
